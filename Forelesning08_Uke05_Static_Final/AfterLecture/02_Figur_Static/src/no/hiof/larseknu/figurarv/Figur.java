1fd8aaa771e9e7eb2ea19e5084cddaf9dc001c9d
Lars Emil <larseknu@hiof.no>
2020-01-29 13:53:18 +0100

    Initielt eksempel til forelesning 08

---
+++ Forelesning08_Uke05_Static_Final/AfterLecture/02_Figur_Static/src/no/hiof/larseknu/figurarv/Figur.java
@@ -0,0 +1,34 @@
+package no.hiof.larseknu.figurarv;
+
+/**
+ * Klasse som representerer en generisk figur
+ */
+public class Figur {
+
+    public double areal() {
+        return 0;
+    }
+
+    public double omkrets() {
+        return 0;
+    }
+
+    /**
+     * Metode for å sammenligne to figurer
+     * Vi kan kalle den direkte med klassen, uten å instansiere ett objekt først, fordi den er statisk
+     */
+    public static boolean erStorre(Figur figur1, Figur figur2)
+    {
+        return figur1.areal() > figur2.areal();
+    }
+
+    /**
+     * Metode for å sammenligne to figurer
+     * Denne er ikke statisk, så vi må ha en instans av Figur-klassen for å kunne kalle den
+     */
+    public boolean erStorreMetodeSomIkkeErStatisk(Figur figur1, Figur figur2)
+    {
+        return figur1.areal() > figur2.areal();
+    }
+
+}
