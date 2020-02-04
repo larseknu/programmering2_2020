4759798f2be9dc10e9691359a773532c7022d653
Lars Emil Knudsen <larsemil87@gmail.com>
2020-02-04 10:06:28 +0100

    Oppdatert forelesning 09

---
+++ Forelesning09_Uke06_Abstract_Interface/BeforeLecture/03_Figur_Interface_Comparable/src/no/hiof/larseknu/figurarv/Main.java
@@ -0,0 +1,22 @@
+package no.hiof.larseknu.figurarv;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Rektangel rektangelet = new Rektangel(4, 5, "Grønn");
+
+        Sirkel sirkelen = new Sirkel(3, "Rød");
+
+        Kvadrat kvaratet = new Kvadrat(5, "Blå");
+
+        System.out.println(rektangelet + " Areal: " + rektangelet.areal() + " Omkrets: " + rektangelet.omkrets());
+        // String.format() gjør det her mulig å runde av double-verdien til å kun vise 2 desimaler
+        System.out.println(String.format("%s Areal: %.2f Omkrets: %.2f", sirkelen, sirkelen.areal(), sirkelen.omkrets()));
+
+        rektangelet.tegn();
+        sirkelen.tegn();
+        kvaratet.tegn();
+    }
+}
